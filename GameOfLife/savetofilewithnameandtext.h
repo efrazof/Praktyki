fe5bcfcb0c13267fb1dbc1f8d210f6cf0689fb6a
KamilaStefaniak <kamila.stefaniak@ericsson.com>
2017-02-22 16:00:37 +0100

    Add test to save file

--- GameOfLife/savetofilewithnameandtext.h
+++ GameOfLife/savetofilewithnameandtext.h
@@ -18,6 +18,10 @@ public:
     void askUserFileName();
     void checkIfFileExist();
     void ifFileExistAskRenameOrOverwritten();
+    std::string getFileName() const;
+    void setFileName(const std::string &value);
+    bool getFileExistStatus() const;
+    void setFileExistStatus(bool value);
 };
 
 #endif // SAVETOFILE_H
