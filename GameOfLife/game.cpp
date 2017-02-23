ff128ecf16e47ffcf8534472d3e1134e7e217bee
Mateusz Mucha <Mateusz.Mucha@ericsson.com>
2017-02-23 08:44:23 +0100

    add menu to game in constructor game

--- GameOfLife/game.cpp
+++ GameOfLife/game.cpp
@@ -3,8 +3,32 @@
 
 Game::Game()
 {
-filler.fillBoard(board);
-startInfiniteLoop();
+        int yourChoice;
+        do{
+
+           std::cout << "What you want to do:\n";
+           std::cout << "1. Load board form file txt\n";
+           std::cout << "2. Load board form file image\n";
+           std::cout << "3. Generating random board\n";
+           std::cout << "0. Exit\n";
+           std::cin >> yourChoice;
+           switch( yourChoice )  {
+           case 2:
+               //Load board from txt method
+               break;
+           case 1:
+               //load board from image method
+               break;
+           case 3:
+               filler.fillBoard(board);
+               startInfiniteLoop();
+               break;
+           case 0:
+               std::cout <<"Goodbye.\n";
+               exit(1);
+               break;
+           }
+        }while (yourChoice != 0);
 }
 
 void Game::startInfiniteLoop()
